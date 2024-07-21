96f897ea5e5c2db2a9ead8110fcf3e52c0da0808
PH <jnes40722@gmail.com>
2024-07-21 15:26:10 +0900

    Add page for korean date

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ st.title("한국숫자 연습")
 
 
 # Function to generate a random number based on a specified digit
-def get_number(digit: int):
+def get_number(digit: int) -> str:
     response = db.get("get_number", params={"digit": digit})
     return response["current_number"]
 
@@ -17,45 +17,83 @@ def get_display_knum(number: int):
 
 
 # Function to fetch a random date
-def get_date(number: int):
-    response = db.post("get_date")
+def get_date() -> str:
+    response = db.get("get_date")
     return response["date"]
 
 
 # Function to generate audio
-def get_audio_path(number: int):
-    response = db.post("play_audios", json={"input_number": str(number)})
+def get_audio_path(input_text: str) -> str:
+    response = db.post("play_audios", json={"input_text": input_text})
     return response.strip('"')
 
 
 # Initialize the client
 db = Client()
 
-# Create a text input box
-digit = st.number_input("자리", min_value=1, max_value=5, value=4)
 
-if "action1" not in st.session_state:
-    st.session_state.action1 = False
+# Page functions:
+# The page for korean number:
+def number_page():
+    # Create a text input box
+    digit = st.number_input("자리", min_value=1, max_value=5, value=4)
 
-# Display button for generating number, Korean word, and audio
-if st.button("TRY IT"):
-    st.session_state.action1 = True
-    # Generate a random number based on a specified digit (e.g., for digit=2, generating 21. )
-    st.session_state.number = get_number(digit)
+    if "action1" not in st.session_state:
+        st.session_state.action1 = False
 
-    # Generate the Korean number for display (e.g., 이십일)
-    st.session_state.display_knum = get_display_knum(st.session_state.number)
+    # Display button for generating number, Korean word, and audio
+    if st.button("TRY IT"):
+        st.session_state.action1 = True
+        # Generate a random number based on a specified digit (e.g., for digit=2, generating 21. )
+        st.session_state.number = get_number(digit)
 
-    # Generate the audio (.mp3 file)
-    st.session_state.audio_path = get_audio_path(st.session_state.number)
+        # Generate the Korean number for display (e.g., 이십일)
+        st.session_state.display_knum = get_display_knum(st.session_state.number)
 
-# Check if the TRY IT button was pressed
-# Keep the random number showing on the page
-if st.session_state.action1:
-    # Display the generated random number on UI (e.g., 21)
-    st.title(f"{st.session_state.number}")
-    # Allow the user to play the audio
-    st.audio(st.session_state.audio_path)
-    # Display the Korean word of the generated random number on UI (e.g., 21)
-    if st.button("Answer"):
-        st.write(f"{st.session_state.display_knum}")
+        # Generate the audio (.mp3 file)
+        st.session_state.audio_path = get_audio_path(st.session_state.number)
+
+    # Check if the TRY IT button was pressed
+    # Keep the random number showing on the page
+    if st.session_state.action1:
+        # Display the generated random number on UI (e.g., 21)
+        st.title(f"{st.session_state.number}")
+        # Allow the user to play the audio
+        st.audio(st.session_state.audio_path)
+        # Display the Korean word of the generated random number on UI (e.g., 21)
+        if st.button("Answer"):
+            st.write(f"{st.session_state.display_knum}")
+
+
+# The page for korean date:
+def date_page():
+    if "action2" not in st.session_state:
+        st.session_state.action2 = False
+
+    # Display button for generating number, Korean word, and audio
+    if st.button("TRY IT"):
+        st.session_state.action2 = True
+        # Generate a random date e.g., 4월24일
+        st.session_state.date = get_date()
+
+        # Generate the audio (.mp3 file)
+        st.session_state.audio_path = get_audio_path(st.session_state.date)
+
+    if st.session_state.action2:
+        # Display the generated random number on UI (e.g., 4월24일)
+        st.title(f"{st.session_state.date}")
+
+        # Allow the user to play the audio
+        st.audio(st.session_state.audio_path)
+
+
+# Page dictionary
+pages = {"Number": number_page, "Date": date_page}
+
+# Sidebar with a drop-down list for navigation
+st.sidebar.title("Navigation")
+selection = st.sidebar.selectbox("Go to", list(pages.keys()))
+
+# Display the selected page
+page = pages[selection]
+page()
